14d92460ffeff9ca003b48852181372d2c84cd80
callmeweirdo <ogbandav102@gmail.com>
2024-10-26 19:26:17 +0100

    made the  cards to be well displayed and shaped

--- components/DevSkill.tsx
+++ components/DevSkill.tsx
@@ -1,4 +1,5 @@
 import type { CardProps } from 'tamagui';
+import { H3 } from 'tamagui';
 import { Button, Card, H2, Image, Paragraph, XStack } from 'tamagui';
 
 export function DevSkill() {
@@ -19,10 +20,10 @@ export function DevSkill() {
           scale={0.9}
           hoverStyle={{ scale: 0.925 }}
           pressStyle={{ scale: 0.875 }}
-          $sm={{ width: '90%', height: 300 }}
-          $md={{ width: '45%', height: 300 }}
-          $lg={{ width: '23%', height: 300 }}  // For large screens
-          $xl={{ width: '23%', height: 300 }}  // For extra-large screens and above
+          $sm={{ width: '90%', height: 270 }}
+          $md={{ width: '45%', height: 270 }}
+          $lg={{ width: '23%', height: 270 }}  // For large screens
+          $xl={{ width: '23%', height: 270 }}  // For extra-large screens and above
         />
       ))}
     </XStack>
@@ -33,7 +34,7 @@ export function DemoCard(props: CardProps) {
   return (
     <Card elevate size="$4" bordered {...props}>
       <Card.Header padded>
-        <H2>Sony A7IV</H2>
+        <H3>Sony A7IV</H3>
         <Paragraph theme="alt2">Now available</Paragraph>
       </Card.Header>
       <Card.Footer padded>
@@ -45,8 +46,8 @@ export function DemoCard(props: CardProps) {
           resizeMode="contain"
           alignSelf="center"
           source={{
-            width: 300,
-            height: 300,
+            width: 250,
+            height: 250,
             uri: 'https://cdn.dribbble.com/userupload/5859589/file/original-55534b3895c5e6fee63696b7418eade7.png?resize=752x',
           }}
         />
